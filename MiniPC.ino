be55a9feee54c73b04aa2779e1feb084f79bf8ff
Arthur <104400862+ArthurHenriqueSchlikmann@users.noreply.github.com>
2024-04-05 19:52:27 -0300

    v1

--- MiniPC.ino
+++ MiniPC.ino
@@ -8,6 +8,8 @@
 #define DPins[4] = {11, 10, 9, 8};
 
 int lig = LOW;
+int err = LOW;
+int btn = LOW;
 
 int table[10] = 
 {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90};
@@ -21,6 +23,7 @@ void setup() {
   pinMode(latch, OUTPUT);
   pinMode(cs, OUTPUT);
   pinMode(data, OUTPUT);
+  pinMode(buzz, OUTPUT);
   for(int i =0; i < 4; i++) {
     pinMode(DPins[i], OUTPUT);
   }
@@ -28,7 +31,12 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int btn = LOW;
+  if(err == HIGH) {
+    digitalWrite(buzz, HIGH);
+    delay(500);
+    digitalWrite(buzz, LOW);
+    err = LOW;
+  }
   btn = digitalRead(botao);
   switch(btn) {
     case 1:
